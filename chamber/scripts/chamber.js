9c106866dec7da0a4efd5312f9ad6a7f38ee6cce
sirdave01 <d08178084956@gmail.com>
2025-11-13 09:14:57 -0800

    still coding

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -29,7 +29,7 @@ document.addEventListener(`DOMContentLoaded`, function () {
 
     if (lastEdited) {
 
-        lastEdited.textContent = `Last Modified: ${new Date().toLocaleString()}`
+        lastEdited.textContent = `Last Modified: ${document.lastModified().toLocaleString()}`
     }
 })
 
@@ -48,6 +48,91 @@ document.addEventListener(`DOMContentLoaded`, () => {
         }
     });
 });
+
+// view toggle for the Directory Page
+
+function initViewSwitch() {
+
+    const gridButton = document.querySelector('#grid-view');
+    const listButton = document.querySelector('#list-view');
+    const container = document.querySelector(`#members-container`);
+
+    if (!gridButton || !listButton || !container) return;
+
+    gridButton.addEventListener('click', () => {
+        gridButton.classList.add('active');
+        container.classList.remove('active');
+    });
+
+    listButton.addEventListener('click', () => {
+        listButton.classList.add('active');
+        container.classList.remove('active');
+    });
+}
+
+// fetch and render membeer data from JSON file for directory page
+
+async function loadMembers() {
+    try {
+        const response = await fetch('data/members.json');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const members = await response.json();
+        renderMembers(members);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        document.querySelector('.members-container').innerHTML = '<p>Failed to load member data.</p>';
+    }
+}
+
+function renderMembers(members) {
+    const container = document.querySelector('#members-container');
+    const searchInput = document.querySelector('#search-input');
+
+    if (!container || !searchInput) return;
+
+    function filterAndDisplayMembers() {
+        const filtered = members.filter(member =>
+            member.name.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+            member.info.toLowerCase().includes(searchInput.value.toLowerCase())
+        );
+        container.innerHTML = filtered.map(member => `
+            <article class="member-card">
+                <img src="${member.image} alt="${member.name} Logo" loading="lazy" onerror="this.src='images/icons/default.png';>
+                <h3>${member.name}</h3>
+                <span class="level-badge">Level ${member.membershipLevel} ${getLevelName(member.membershipLevel)}</span>
+                <p><strong>Address:</strong>${member.address}</p>
+                <p><strong>Phone:</strong> ${member.phoneNumber}</p>
+                <p><strong>Website:</strong> <a href="https://${member.website}" target="_blank" rel="noopener">${member.website}</a></p>
+                <p>${member.Info}</p>
+            </article>
+        `).join('');
+    }
+
+    // inital display
+    filterAndDisplayMembers();
+
+    // add event listener for search input
+    if (searchInput) {
+        searchInput.addEventListener('input', (e) => filterAndDisplayMembers(e.target.value));
+    }
+}
+
+function getLevelName(membershipLevel) {
+    return membershipLevel === 1 ? `member` : membershipLevel === 2 ? `silver` : membershipLevel === 3 ? `gold` : ``;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 // dark mode toggle functionality
 
@@ -74,4 +159,9 @@ function toggleDarkMode() {
     toggleSwitch.textContent = newMode === 'dark' ? '☀️' : '🌙';
 }
 
-document.addEventListener('DOMContentLoaded', initDarkMode);
+// eventlistener for all the functions to load when the DOM content is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    initDarkMode();
+    initViewSwitch();
+    loadMembers();
+});
